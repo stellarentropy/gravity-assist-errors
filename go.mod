3f9d407926c07d9b81c44a70539fa9fb552b1a28
Henrique Goncalves <kamus@hadenes.io>
2023-11-29 08:52:57 -0300

    add logging

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.1
 
 require (
 	github.com/google/uuid v1.4.0
+	github.com/rs/zerolog v1.31.0
 	github.com/segmentio/ksuid v1.0.4
 	github.com/stellarentropy/uuid v0.0.0-20231027224247-2ba7682b6409
 	github.com/stretchr/testify v1.8.4
@@ -11,7 +12,10 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stellarentropy/isaac64 v0.0.0-20231027223639-d0cef4761baf // indirect
+	golang.org/x/sys v0.12.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ logging/logging.go
@@ -0,0 +1,70 @@
+package logging
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/diode"
+)
+
+// GetLogger returns a preconfigured [zerolog.Logger] for application-wide
+// logging, ensuring timestamped and formatted output with efficient message
+// processing, including the ability to drop messages under high load to
+// maintain system performance.
+func GetLogger() zerolog.Logger {
+	// Set the global duration field unit to milliseconds
+	zerolog.DurationFieldUnit = time.Millisecond
+	// Set the global time field format to RFC3339
+	zerolog.TimeFieldFormat = time.RFC3339
+
+	// Declare a variable to hold the multi-level writer
+	var multi zerolog.LevelWriter
+
+	// TODO: add check to log the more efficient raw json format when running in production
+
+	// Create a new diode writer with a buffer size of 1000 and a flush interval of 10 milliseconds
+	wr := diode.NewWriter(consoleWriter(), 1000, 10*time.Millisecond, func(missed int) {
+		// If any messages are dropped, print the number of dropped messages
+		fmt.Printf("Dropped %d messages", missed)
+	})
+
+	// Assign the diode writer to the multi-level writer
+	multi = zerolog.MultiLevelWriter(wr)
+
+	// Create a new logger with the multi-level writer, add a timestamp to each log message
+	logger := zerolog.New(multi).With().Timestamp().Logger()
+
+	// Set the global log level to Info
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	// Return the logger
+	return logger
+}
+
+// consoleWriter creates and returns a [zerolog.ConsoleWriter] that formats log
+// messages for display in the console, incorporating features like color
+// coding, time stamps, caller information, log levels, and message content
+// arranged in a readable tabular format with fixed message width.
+func consoleWriter() zerolog.ConsoleWriter {
+	// Create a new console writer that outputs to the standard output
+	writer := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "15:04:05", NoColor: false}
+
+	// Set the order of the parts in the log message
+	writer.PartsOrder = []string{
+		zerolog.TimestampFieldName,
+		zerolog.CallerFieldName,
+		zerolog.LevelFieldName,
+		zerolog.MessageFieldName,
+	}
+
+	// Define the format of the message part
+	writer.FormatMessage = func(i interface{}) string {
+		// Format the message to have a fixed width of 60 characters
+		return fmt.Sprintf("| %-60s|", i)
+	}
+
+	// Return the configured console writer
+	return writer
+}
