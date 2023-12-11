576543bfe684d90be45e9866f0af47fe4abfb50a
Henrique Goncalves <kamus@hadenes.io>
2023-12-11 19:12:44 -0300

    remove metric deadlock

--- metrics/tracer/metric.go
+++ metrics/tracer/metric.go
@@ -12,11 +12,12 @@ var metrics = make(map[string]metric.Meter)
 
 var counters = make(map[string]metric.Int64Counter)
 
-var lock = sync.Mutex{}
+var metricsLock = sync.Mutex{}
+var countersLock = sync.Mutex{}
 
 func NewMetric(ctx context.Context, component string, opts ...metric.MeterOption) metric.Meter {
-	lock.Lock()
-	defer lock.Unlock()
+	metricsLock.Lock()
+	defer metric.Unlock()
 
 	if m, ok := metrics[component]; ok {
 		return m
@@ -30,8 +31,8 @@ func NewMetric(ctx context.Context, component string, opts ...metric.MeterOption
 }
 
 func AddInt64(ctx context.Context, component string, name string, value int64, opts ...metric.AddOption) error {
-	lock.Lock()
-	defer lock.Unlock()
+	countersLock.Lock()
+	defer countersLock.Unlock()
 
 	m := NewMetric(ctx, component)
 
