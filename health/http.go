207ec4656c4448378469f433978be72014f693b8
Henrique Goncalves <kamus@hadenes.io>
2023-12-11 18:27:57 -0300

    add health server

---
+++ health/http.go
@@ -0,0 +1,110 @@
+package health
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+
+	"github.com/stellarentropy/gravity-assist-common/config/common"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stellarentropy/gravity-assist-common/errors"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+var ready = false
+
+func Ready() {
+	ready = true
+}
+
+func getRouter() *chi.Mux {
+	router := chi.NewRouter()
+
+	router.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		defer tracerNewSpan(r.Context(), "healthz.request").End()
+
+		_ = tracerAddInt64(r.Context(), "healthz.request.count", 1)
+
+		_, _ = w.Write([]byte("OK"))
+	})
+
+	router.Get("/readyz", func(w http.ResponseWriter, r *http.Request) {
+		defer tracerNewSpan(r.Context(), "readyz.request").End()
+
+		_ = tracerAddInt64(r.Context(), "readyz.request.count", 1)
+
+		if ready {
+			_, _ = w.Write([]byte("OK"))
+		} else {
+			w.WriteHeader(http.StatusServiceUnavailable)
+		}
+	})
+
+	logger.LogRoutes(router)
+
+	return router
+}
+
+func Listen(ctx context.Context) error {
+	listener, err := net.Listen("tcp",
+		net.JoinHostPort(
+			common_config.Common.HealthListenAddress,
+			fmt.Sprintf("%d", common_config.Common.HealthListenPort)))
+	if err != nil {
+		return err
+	}
+
+	// Log the starting of the health server
+	logger.Info().
+		Str("address", listener.Addr().(*net.TCPAddr).IP.String()).
+		Int("port", listener.Addr().(*net.TCPAddr).Port).
+		Msg("starting health server")
+
+	router := getRouter()
+
+	// Get the address of the server
+	addr := listener.Addr().(*net.TCPAddr).String()
+
+	server := &http.Server{
+		Addr:         addr,
+		Handler:      h2c.NewHandler(router, &http2.Server{}),
+		ReadTimeout:  common_config.Common.HealthReadTimeout,
+		WriteTimeout: common_config.Common.HealthWriteTimeout,
+	}
+
+	// Start a new goroutine that listens for the context cancellation signal
+	go func() {
+		// Wait for the context to be done
+		<-ctx.Done()
+		// Log the shutting down of the health server
+		logger.Info().Msg("shutting down health server")
+
+		tctx, cancel := context.WithTimeout(context.Background(), common_config.Common.GracefulShutdownTimeout)
+		defer cancel()
+
+		// Attempt to gracefully shut down the server and log any errors
+		if err := server.Shutdown(tctx); err != nil {
+			logger.Error().Err(err).Msg("error shutting down health server")
+		}
+	}()
+
+	// Start the server and return any errors encountered
+	return server.Serve(listener)
+}
+
+func Start(ctx context.Context, wg *sync.WaitGroup) {
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		if err := Listen(ctx); err != nil && !errors.Is(err, context.Canceled) && !errors.Is(err, http.ErrServerClosed) {
+			logger.Error().Err(err).Msg("error in health server")
+			panic(err)
+		}
+	}()
+}
