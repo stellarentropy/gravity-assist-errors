f37c236bcbf5e5fdec5578231369c260e92467a9
Henrique Goncalves <kamus@hadenes.io>
2023-12-11 21:27:54 -0300

    update NewSpan

--- metrics/tracer/timing.go
+++ metrics/tracer/timing.go
@@ -3,17 +3,15 @@ package tracer
 import (
 	"context"
 
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 )
 
 func NewSpan(ctx context.Context, name string, attributes ...attribute.KeyValue) (context.Context, trace.Span) {
-	span := trace.SpanFromContext(ctx)
-	span.SetName(name)
+	ctx, span := otel.GetTracerProvider().Tracer("gravity-assist").Start(ctx, name)
 
 	span.SetAttributes(attributes...)
-
-	ctx = trace.ContextWithSpan(ctx, span)
 
 	return ctx, span
 }
